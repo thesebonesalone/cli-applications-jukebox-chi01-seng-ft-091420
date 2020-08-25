d7e7921e752c398b886265bb4c87f39881eb48ab
Willem A Duiker <duikhead2@gmail.com>
2020-08-25 18:43:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -6,7 +6,5 @@ def help
   puts "help : displays this help message"
   puts "list : displays a list of songs you can play"
   puts "play : lets you choose a song to play"
-  puts "exit : "
-  
-  
+  puts "exit : exits this program"
 end
