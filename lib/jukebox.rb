ddb512e17c713b54e3c45fbcd60e6a51792f5e22
Willem A Duiker <duikhead2@gmail.com>
2020-08-25 18:43:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -4,7 +4,7 @@
 def help
   puts "I accept the following commands:"
   puts "help : displays this help message"
-  puts "list : "
+  puts "list : displays a list of songs you can play"
   puts "play : "
   puts "exit : "
   
